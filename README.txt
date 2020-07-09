90e1d974a553177ea3d2f0742ca5c3fe49a17b0c
Trung Trinh <trung28899@gmail.com>
2020-07-09 17:38:31 -0400

    declare, get and update state in react hooks

---
+++ README.txt
@@ -0,0 +1,61 @@
+1. Tools used: 
+
+2. Core Knowledge: 
+
+a. Types of component before react hooks: 
+- Class based component:
+    +, Takes Props in, return JSX out
+    +, Great for presentation
+    +, Focused on limited purpose, mostly 
+    rendering component only
+
+- Functional component: 
+    +, Uses props and state
+    +, Business logic goes in here
+    +, Orchestrates components
+
+In general, Functional component can't have states and lifecyle 
+methods yet while Class-based can 
+
+b. What is React-Hooks: 
+
+- React Hooks is an approach that allows us to work with
+Functional component ONLY while still able to maintain 
+state and lifecyle hooks. 
+
+- React Hooks: 
+    +, is introduced with React 16.8
+    +, Allow dev to use Functional components only
+    +, Has Hooks for managing states and side effects
+    (HTTP requests, etc...)
+    +, Allow dev to build custom hooks to share stateful
+    or stateless logic across multiple components
+
+- Naming React rules: 
+    +, named: useXYZ(). XYZ is the function's name
+
+c. useState() function return: 
+- useState() always return an array with 2 elements: 
+    +, 1st element is your current state snapshot
+    +, 2nd element is a function that allows you to update
+    your state
+
+3. Guide on how to use this module:
+
+VER 1: useState() to set, get and update
+state properly in React
+-------------------------------------------------------------------
+Code in 2nd commit
+
+Step 1: Go to components/Ingredients/IngredientsForm.js
+Step 2: See how to DECLARE STATE with the declaration of 
+'inputState'
+Step 3: Look into the two <input> tags, see 'value' attributes
+to see how to GET STATE 
+Step 4: Look into the two <input> tags, see the 'onChange' attributes
+to see the 2 ways of updating state
+    +, <input> of 'Name': updating state, howevers, might
+    have unpredicted event of updating the other box 
+    +, <input> of 'Amount': updating state properly, follow
+    exactly this approach all the time for perfect state 
+    updating
--- src/components/Ingredients/IngredientForm.js
+++ src/components/Ingredients/IngredientForm.js
@@ -1,10 +1,22 @@
-import React from 'react';
+import React, { useState } from "react";
 
-import Card from '../UI/Card';
-import './IngredientForm.css';
+import Card from "../UI/Card";
+import "./IngredientForm.css";
 
-const IngredientForm = React.memo(props => {
-  const submitHandler = event => {
+// Review React.memo in component dive deep
+// Anytime the state changes, the whole function
+// will get rebuilted
+const IngredientForm = React.memo((props) => {
+  // state declaration in functional
+  /*
+    useState() always return an array with 2 elements: 
+    +, 1st element is your current state snapshot
+    +, 2nd element is a function that allows you to update
+    your state
+  */
+  const inputState = useState({ title: "", amount: "" });
+
+  const submitHandler = (event) => {
     event.preventDefault();
     // ...
   };
@@ -15,11 +27,32 @@ const IngredientForm = React.memo(props => {
         <form onSubmit={submitHandler}>
           <div className="form-control">
             <label htmlFor="title">Name</label>
-            <input type="text" id="title" />
+            <input
+              type="text"
+              id="title"
+              value={inputState[0].title}
+              onChange={(event) =>
+                inputState[1]({
+                  title: event.target.value,
+                  amount: inputState[0].amount,
+                })
+              }
+            />
           </div>
           <div className="form-control">
             <label htmlFor="amount">Amount</label>
-            <input type="number" id="amount" />
+            <input
+              type="number"
+              id="amount"
+              value={inputState[0].amount}
+              onChange={(event) => {
+                const newAmount = event.target.value;
+                inputState[1]((prevInputState) => ({
+                  amount: newAmount,
+                  title: prevInputState.title,
+                }));
+              }}
+            />
           </div>
           <div className="ingredient-form__actions">
             <button type="submit">Add Ingredient</button>
